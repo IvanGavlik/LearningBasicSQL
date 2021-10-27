fdf95fc4c71d4dc84035f8109298a4487542e313
IvanGavlik <ivangavlik963@gmail.com>
2021-10-28 00:52:30 +0200

    create relation many to many actor-film and first insert

--- createTables/createTables.sql
+++ createTables/createTables.sql
@@ -1,4 +1,41 @@
-
 /*
- Create tables
+ Create tables actor, fi
  */
+ CREATE TABLE sakila.actor(
+    actor_id serial primary key,
+    first_name varchar(45),
+    last_name varchar(45),
+    last_update timestamp not null
+);
+
+create table sakila.film_actor (
+    actor_id integer,
+    film_id integer,
+    last_updated timestamp,
+    primary key (actor_id, film_id)
+)
+
+create type sakila.rating as enum ('G', 'PG', 'PG-13', 'R', 'NC-17' );
+create table sakila.film(
+  film_id serial primary key, 
+  title varchar(255) not null,
+  description text,
+  release_year smallint,
+  rental_duration interval not null,
+  rental_rate decimal(4, 2) not null,
+  length smallint,
+  replacement_cost decimal(5, 2) not null,
+  rating rating,
+  special_features text,
+  last_update timestamp not null
+);
+
+-- set relationships many to many on film - actor (film_actor)
+alter table sakila.film_actor
+add constraint fk_actor_film_actor
+foreign key (actor_id) references sakila.actor (actor_id) on delete restrict on update cascade;
+
+alter table sakila.film_actor
+add constraint fk_actor_film_film
+foreign key (film_id) references sakila.film (film_id) on delete restrict on update cascade;
+
---
+++ insertData/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO sakila.film VALUES (190,
+                                'CREEPERS KANE',
+                                'A Awe-Inspiring Reflection of a Squirrel And a Boat who must Outrace a Car in A Jet Boat',
+                                2006,
+                                '1',
+                                4.99,
+                                172,
+                                '23.99',
+                                'NC-17',
+                                'Trailers,Behind the Scenes',
+                                '2006-02-15 05:03:42');
